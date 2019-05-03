d442a3b001c8dc1f50661355678dc097ecf27a2e
Paweł Szynkiewicz <sed s/x/pszynk/  x@gmail.com>
2019-05-04 00:08:56 +0200

    use zstyle instead of config file

--- zsh-dircolors.plugin.zsh
+++ zsh-dircolors.plugin.zsh
@@ -1,30 +1,25 @@
-# get the directory from when we're sourced
-_ZSH_DIRCOLORS_DIR="${ZSH_DIRCOLORS_DIR:-${${0:A}:h}/dircolors}"
-_ZSH_DIRCOLORS_CONF="${ZSH_DIRCOLORS_CONF:-$HOME/.zsh-dircolors.config}"
 
-function lsdircolors ()
-{
-    for i in ${_ZSH_DIRCOLORS_DIR}/dircolors.*
-    do
-        echo $(basename ${i})
-    done
+# Return if requirements are not found.
+if (( ! $+commands[dircolors] )); then
+  return 1
+fi
+
+function _setup_dircolors {
+  local dircolors_dir
+  local theme
+  local theme_path
+
+  dircolors_dir="${ZSH_DIRCOLORS_DIR:-${0:A:h}/dircolors}"
+  zstyle -s ':pszynk:module:dircolors' theme 'theme' || theme='dircolors.ansi-universal'
+  theme_path="${dircolors_dir}/${theme}"
+
+  if [[ -f $theme_path ]]; then
+    eval $(dircolors $theme_path)
+    return 0
+  fi
+
+  print "Could not load dircolors theme, file not found: '$theme_path'" >&2
+  return 1
 }
 
-function setupdircolors ()
-{
-    local _dircolors_theme=${1:-'dircolors.ansi-universal'}
-
-    local _dircolors_theme_path="$_ZSH_DIRCOLORS_DIR/${_dircolors_theme}"
-    if [[ -f $_dircolors_theme_path ]]; then
-        eval $(dircolors $_dircolors_theme_path)
-    else
-        echo "Could not load dircolors theme: '$_dircolors_theme_path'"
-    fi
-
-    # Save the settings to a file
-    setopt CLOBBER
-    echo $_dircolors_theme > $_ZSH_DIRCOLORS_CONF
-    unsetopt CLOBBER
-}
-
-[[ -e $_ZSH_DIRCOLORS_CONF ]] && setupdircolors $(cat $_ZSH_DIRCOLORS_CONF)
+_setup_dircolors $@
